675916e66038a188ff14e1fb445cc8e0df02fb95
Serhat <s.ciftci561@gmail.com>
2025-02-23 21:31:27 +0300

    delete,update,post,get islemleri tamamlandı

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -49,7 +49,71 @@ namespace ProductsAPI.Controllers
             }
             return Ok(p);
         }
-        
+        //veri ekleme
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(Product entity)
+        {
+          _context.Products.Add(entity);
+          await _context.SaveChangesAsync();
+          // ilk olarak hangi action methoda giticek sonra , ile new yazarak id bilgisini gönderiyoruz idye karşılık değer gelende entytyProdcutId olacak son parametre ise hangi entity eklendi o.
+          return CreatedAtAction(nameof(GetProducts),new {id=entity.ProductId},entity);
+        }
+        //veri güncelleme
+        [HttpPut("{id}")]
+        public async Task <IActionResult> UpdateProduct(int id,Product entity)
+    {
+            
+            if(id!=entity.ProductId)
+            {
+                return BadRequest(); //400 
+            }
+            var product= await _context.Products.FirstOrDefaultAsync(x=>x.ProductId==id);
+            if(product==null)
+            {
+                return NotFound();//404 
+            }
+            product.ProductName=entity.ProductName;
+            product.Price=entity.Price;
+            product.IsActive=entity.IsActive;
+            ;
+            try 
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+            return NoContent();//204 herşey normal geriye birşey döndürmüyoruz günceleme işlemi başarılı
     }
     
+    /// <summary>
+    /// Ürün Silme İşlemi
+    /// </summary>
+    /// <param name="id">Silinecek Ürün Idsi</param>
+    /// <returns></returns>
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(int? id)
+    {
+        if(id==null)
+        {
+            return NotFound();
+        }
+        var product= await _context.Products.FirstOrDefaultAsync(x=>x.ProductId==id);
+        if(product==null)
+        {
+            return NotFound();
+        }
+        _context.Products.Remove(product);
+        try{
+            await _context.SaveChangesAsync();
+        }
+        catch(Exception)
+        {
+            return NotFound();
+        }
+        return Ok(new{message="Ürün başarıyla silindi",deletedProductId=product});
+    }
+    
+ }
  }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using ProductsAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
+// Swagger için XML dokümantasyonu ekleyelim
+var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+builder.Services.AddSwaggerGen(c =>
+{
+    c.IncludeXmlComments(xmlPath);
+});
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<ProductsContext>(options =>
